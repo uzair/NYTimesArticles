7e840ae1fdfdf902b6f667806d9a334eeac5eb3d
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-02-15 14:24:21 +0400

    Loaded data on table view

---
+++ NYTimesArticles/Scenes/ArticleList/Presentation/UI/Cells/ArticleCell.swift
@@ -0,0 +1,23 @@
+//
+//  ArticleCell.swift
+//  NYTimesArticles
+//
+//  Created by Macbook on 15/02/2024.
+//
+
+import UIKit
+
+class ArticleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
