8da4a92dbe06bf0542aecbe50a4aa224a34c8995
Macbook <uzair_shahzad@hotmail.com>
2024-02-17 22:58:13 +0400

    Fixed the broken tests.

--- NYTimesArticlesTests/ArticleListViewModelTests/ArticleListViewModelTests.swift
+++ NYTimesArticlesTests/ArticleListViewModelTests/ArticleListViewModelTests.swift
@@ -13,7 +13,7 @@ import XCTest
 
 final class MockGetArticlesMetaDataUseCase: GetArticlesMetaDataUseCaseContractor {
     var getArticleListCalled = false
-
+    
     func getArticleList(completion: @escaping (Result<ArticlesMetaData, ServiceError>) -> Void) {
         getArticleListCalled = true
         // Simulate a response for testing
@@ -31,7 +31,7 @@ final class MockErrorGetArticlesMetaDataUseCase: GetArticlesMetaDataUseCaseContr
 
 final class MockViewModelMapper: ArticleListViewModelMapperInterface {
     var listDisplayItemCalled = false
-
+    
     func listDisplayItem(articlesMetaData: ArticlesMetaData) -> ArticleListDisplayItem {
         listDisplayItemCalled = true
         // Simulate a display item for testing
@@ -39,38 +39,29 @@ final class MockViewModelMapper: ArticleListViewModelMapperInterface {
     }
 }
 
-final class MockCoordinatorResolver: CoordinatorResolverContractor {
-    var navigationController: UINavigationController?
-    
-    func articleListCoordinator() -> Coordinator? {
-        let mockCoordinator = MockCoordinator.shared
-        mockCoordinator.resolver = self
-        return mockCoordinator
-    }
-    
-    func articleDetailCoordinator(urlString: String?) -> Coordinator? {
-        let mockCoordinator = MockCoordinator.shared
-        mockCoordinator.resolver = self
-        return mockCoordinator
+final class MockArticleRouter: ArticleRouterContractor {
+    var showDetailCalled = false
+    func showDetail(articleDetailUrl: String?) {
+        showDetailCalled = true
     }
 }
 
-final class MockCoordinator: Coordinator {
-    
-    static let shared = MockCoordinator()
-    
-    var resolver: CoordinatorResolverContractor?
-    var startCalled = false
-    
-    init(resolver: CoordinatorResolverContractor? = MockCoordinatorResolver(), startCalled: Bool = false) {
-        self.resolver = resolver
-        self.startCalled = startCalled
-    }
-    
-    func start() {
-        startCalled = true
-    }
-}
+//final class MockCoordinator: Coordinator {
+//
+//    static let shared = MockCoordinator()
+//
+//    var resolver: CoordinatorResolverContractor?
+//    var startCalled = false
+//
+//    init(resolver: CoordinatorResolverContractor? = MockCoordinatorResolver(), startCalled: Bool = false) {
+//        self.resolver = resolver
+//        self.startCalled = startCalled
+//    }
+//
+//    func start() {
+//        startCalled = true
+//    }
+//}
 
 final class ArticleListViewModelTests: XCTestCase {
     
@@ -82,74 +73,74 @@ final class ArticleListViewModelTests: XCTestCase {
     }
     
     // MARK: - Tests
-
-      func testOnViewLoadCallsLoadArticlesMetaData() {
-          // Given
-          let mockUseCase = MockGetArticlesMetaDataUseCase()
-          articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, coordinator: nil)
-
-          // When
-          articleListViewModel.onViewLoad()
-
-          // Then
-          XCTAssertTrue(mockUseCase.getArticleListCalled)
-      }
+    
+    func testOnViewLoadCallsLoadArticlesMetaData() {
+        
+        // Given
+        let mockUseCase = MockGetArticlesMetaDataUseCase()
+        articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, router: nil)
+        
+        // When
+        articleListViewModel.onViewLoad()
+        
+        // Then
+        XCTAssertTrue(mockUseCase.getArticleListCalled)
+    }
     
     func testOnViewLoadUpdatesViewState() {
-          // Given
-          let mockUseCase = MockGetArticlesMetaDataUseCase()
-          articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, coordinator: nil)
-
-          // When
-            articleListViewModel.onViewLoad()
-
-          // Then
+        
+        // Given
+        let mockUseCase = MockGetArticlesMetaDataUseCase()
+        articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, router: nil)
+        
+        // When
+        articleListViewModel.onViewLoad()
+        
+        // Then
         XCTAssertEqual(articleListViewModel.viewState.value, .loading, "onViewLoad should update viewState to .loading")
-      }
+    }
     
     func testOnLoadArticlesMetaDataSuccess() {
-            // Given
-            let mockUseCase = MockGetArticlesMetaDataUseCase()
-            let mockMapper = MockViewModelMapper()
-            articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: mockMapper, coordinator: nil)
-
-            // When
+        
+        // Given
+        let mockUseCase = MockGetArticlesMetaDataUseCase()
+        let mockMapper = MockViewModelMapper()
+        articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: mockMapper, router: nil)
+        
+        // When
         articleListViewModel.onViewLoad()
-
-            // Then
-            XCTAssertTrue(mockMapper.listDisplayItemCalled)
-            XCTAssertEqual(articleListViewModel.viewState.value, .listingArticles(ArticleListDisplayItem(cellDisplayItems: [], title: "")))
-        }
-
-    
-        func testOnLoadArticlesMetaDataFailure() {
-            // Given
-            let mockUseCase = MockErrorGetArticlesMetaDataUseCase()
-            articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, coordinator: nil)
-
-            // When
-            
-            articleListViewModel.onViewLoad()
-
-            // Then
-            XCTAssertEqual(articleListViewModel.viewState.value, .error(ServiceError.requestFailed(nil)))
-        }
-
-    
-        func testOnClickListItemAt() {
-            // Given
-            let mockCoordinator = MockCoordinator()
-            articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: MockGetArticlesMetaDataUseCase(), mapper: nil, coordinator: mockCoordinator)
-
-            // When
-            articleListViewModel.onClickListItemAt(index: 0)
-
-            // Then
-            let nextCoordinator = mockCoordinator.resolver?.articleDetailCoordinator(urlString: "") as! MockCoordinator
-            XCTAssertNotNil(nextCoordinator)
-            XCTAssertTrue(nextCoordinator.startCalled)
-        }
+        
+        // Then
+        XCTAssertTrue(mockMapper.listDisplayItemCalled)
+        XCTAssertEqual(articleListViewModel.viewState.value, .listingArticles(ArticleListDisplayItem(cellDisplayItems: [], title: "")))
+    }
     
     
+    func testOnLoadArticlesMetaDataFailure() {
+        
+        // Given
+        let mockUseCase = MockErrorGetArticlesMetaDataUseCase()
+        articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: mockUseCase, mapper: nil, router: nil)
+        
+        // When
+        articleListViewModel.onViewLoad()
+        
+        // Then
+        XCTAssertEqual(articleListViewModel.viewState.value, .error(ServiceError.requestFailed(nil)))
+    }
+    
+    
+    func testOnClickListItemAt() {
+        
+        // Given
+        let mockRouter = MockArticleRouter()
+        articleListViewModel = ArticleListViewModel(getArticlesMetaDataUseCase: MockGetArticlesMetaDataUseCase(), mapper: nil, router: mockRouter)
+        
+        // When
+        articleListViewModel.onClickListItemAt(index: 0)
+        
+        // Then
+        XCTAssertTrue(mockRouter.showDetailCalled)
+    }
 }
 
